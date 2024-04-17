d1afc3e807563810a1f4324e253ed5914dcad88f
Nordfjord24 <67027614+Nordfjord24@users.noreply.github.com>
2024-04-17 14:33:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,21 +15,28 @@ def scan_website():
     api_key = 'D5QVJKKD7EJ1DJRQ3GSZ7617L3U6JENC10YZXGF223HPLNKWXW6FJCP9QU6ZGMW006UJKOWMS98D2CCM'
     response = requests.get(f"https://app.scrapingbee.com/api/v1/", params={'api_key': api_key, 'url': url_to_scan})
     
-    #Google Tag Manager Check
-    gtm_id = "Not Found"  # Default value if not found
+    # Check if the request was successful
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        # Find GTM ID using regular expression
-        matches = re.findall(r'googletagmanager\.com/ns\.html\?id=(GTM-\w+)', response.text)
-        if matches:
-            gtm_id = matches[0]  # Assuming the first match is the desired GTM ID
+        # Parse the JSON data
+        json_data = response.json()
+        
+        # Get the HTML content
+        html_content = json_data['body']
+        
+        # Google Tag Manager Check
+        gtm_id = "Not Found"  # Default value if not found
+        if html_content:
+            soup = BeautifulSoup(html_content, 'html.parser')
+            # Find GTM ID using regular expression
+            matches = re.findall(r'googletagmanager\.com/ns\.html\?id=(GTM-\w+)', html_content)
+            if matches:
+                gtm_id = matches[0]  # Assuming the first match is the desired GTM ID
 
-    # Enhanced check for Cookie Consent Tools/Platforms
+        # Enhanced check for Cookie Consent Tools/Platforms
         cookie_consent_keywords = ["cookieconsent", "cookiebot", "cookiehub", "consent"]
         cookie_consent = any(bool(soup.find_all(string=lambda text: keyword in text.lower())) for keyword in cookie_consent_keywords)
 
-        
-    # Pass the GTM ID and cookie consent check result to the template
+        # Pass the GTM ID and cookie consent check result to the template
         return render_template('results.html', url=url_to_scan, gtm_id=gtm_id, cookie_consent=cookie_consent)
     else:
         return f"Failed to fetch website: {response.status_code}"
